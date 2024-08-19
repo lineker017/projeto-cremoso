09122315553ae91e93b848bfe1eb87ba3059a979
lak017 <160450395+lak017@users.noreply.github.com>
2024-08-19 12:24:06 -0300

    finalizado carrossel com js

--- home/script.js
+++ home/script.js
@@ -10,5 +10,32 @@ menuButton.addEventListener("click", () => {
 
 //adicionando funçao de fechar o menu
 closeButton.addEventListener("click", () => {
-    menuModal.classList.remove("active");
+  menuModal.classList.remove("active");
 });
+
+let currentSlide = 0
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".carousel-item")
+
+  if(index >= slides.length) {
+    currentSlide = 0
+  } else if(index < 0) {
+    currentSlide = slides.length - 1
+  } else {
+    currentSlide = index
+  }
+
+  const offset = -currentSlide *100
+  document.querySelector(".carousel-content").style.transform = `translateX(${offset}%)`
+}
+
+function nextSlide() {
+  showSlide(currentSlide +1)
+}
+
+function prevSlide() {
+  showSlide(currentSlide -1)
+}
+
+
